2a09d9907f426b855082f9e1f8198d7ac0d86ec5
xd00258 <victoriquevuong@gmail.com>
2018-06-13 19:56:34 -0400

    initial commit with dataset set up

---
+++ data_processing_template.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv('Data.csv')
+
+x=dataset.iloc[:, :-1].values
+y=dataset.iloc[:, 3].values
+
+print (dataset)
+
