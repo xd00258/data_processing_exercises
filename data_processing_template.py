6eeed24d49613cdffb02f38272bf887177b1b01c
xd00258 <victoriquevuong@gmail.com>
2018-06-13 23:35:49 -0400

    Splitting data into training and test set, and scaling variables

--- data_processing_template.py
+++ data_processing_template.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 dataset = pd.read_csv('Data.csv')
 
+#PREPROCESSING DATA
 x=dataset.iloc[:, :-1].values
 y=dataset.iloc[:, 3].values
 
@@ -21,8 +22,23 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 x[:,0] = labelencoder_X.fit_transform(x[:,0])
 
+#France, Spain, Germany 3 bits with 1 and 0 to represent each: dummy variable
 onehotencoder = OneHotEncoder(categorical_features = [0])
 x = onehotencoder.fit_transform(x).toarray()
 
+#Yes and No
 labelencoder_y = LabelEncoder()
 y= labelencoder_y.fit_transform(y)
+
+#Splitting dataset into training set and test set: do not want model to learn stuff by heart, gotta test with other data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0) #20%
+
+#Feature scaling
+#Standardisastion or normalisation
+#puttin variables in same range
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
