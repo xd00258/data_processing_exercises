05635eb183a09b337864a7670cd78f89f4663e42
xd00258 <victoriquevuong@gmail.com>
2018-06-13 23:13:25 -0400

    facing missing data and encoding categorical data

--- data_processing_template.py
+++ data_processing_template.py
@@ -6,5 +6,23 @@ dataset = pd.read_csv('Data.csv')
 x=dataset.iloc[:, :-1].values
 y=dataset.iloc[:, 3].values
 
-print (dataset)
+#missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
 
+print (imputer)
+
+print (x)
+
+#encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+x[:,0] = labelencoder_X.fit_transform(x[:,0])
+
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x = onehotencoder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y= labelencoder_y.fit_transform(y)
