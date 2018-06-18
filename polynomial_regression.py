ea7e29e0e6e2951709e0b98bc73ad1d9955918a8
xd00258 <victoriquevuong@gmail.com>
2018-06-18 00:05:35 -0400

    fitting and visualizing linear vs polynomial

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -22,3 +22,32 @@ Y = dataset.iloc[:, 2].values
 #no feature scaling needed
 
 
+#---Fitting with linear regression---
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, Y)
+
+#---Fitting with polynomial regression---
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, Y)
+
+#---Visualizing Linear Regression---
+plt.scatter(X, Y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Linear Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+#---Visalizing Polynomial Regression---
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Polynomial Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
