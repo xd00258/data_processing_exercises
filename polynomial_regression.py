b10f9e3edc7f98d4e78a6b79bfc865376d6f5324
xd00258 <victoriquevuong@gmail.com>
2018-06-18 21:04:34 -0400

    predicting for each regression to see difference

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -51,3 +51,9 @@ plt.title('Polynomial Regression')
 plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
+
+#---Predicting new result with Linear Regression---
+lin_reg.predict(6.5)
+
+#---Predicting new result with Polynomial Regression---
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
